539c49360e5e67e82b052f4987a0c67c79bfecdc
Bárbara Abelha <146784507+Abelha09@users.noreply.github.com>
2025-08-18 18:01:15 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FinanceChat from './FinanceChat';
+import './style.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FinanceChat />
+  </React.StrictMode>
+);
